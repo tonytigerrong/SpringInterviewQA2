4cd2b0043a30708d1d4babacc7916a11470fb9d9
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-07-02 11:36:46 -0300

    spring init

---
+++ src/main/java/com/spring/interview/SpringInterviewQa2Application.java
@@ -0,0 +1,13 @@
+package com.spring.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInterviewQa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInterviewQa2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/interview/SpringInterviewQA2/SpringInterviewQa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.interview.SpringInterviewQA2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInterviewQa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
